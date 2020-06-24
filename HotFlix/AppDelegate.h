7b49c572eaa605a4a95a49b057ea36807fd5f743
Mercy Bickell <mercycat@fb.com>
2020-06-24 10:46:31 -0600

    Initial Commit

---
+++ HotFlix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HotFlix
+//
+//  Created by Mercy Bickell on 6/24/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HotFlix/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  HotFlix
+//
+//  Created by Mercy Bickell on 6/24/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
