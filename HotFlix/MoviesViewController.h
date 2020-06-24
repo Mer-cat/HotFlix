4ab43f5d72e20a247bc0601da3ab2ea2e36871aa
Mercy Bickell <mercycat@fb.com>
2020-06-24 11:26:55 -0600

    Added image assets and configured app icon as well as launch screen, also started organizing project files

---
+++ HotFlix/MoviesViewController.h
@@ -0,0 +1,17 @@
+//
+//  MoviesViewController.h
+//  HotFlix
+//
+//  Created by Mercy Bickell on 6/24/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MoviesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
