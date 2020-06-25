cd1eaeb36126a24716c3a37af4426687e4a9a79a
Mercy Bickell <mercycat@fb.com>
2020-06-25 15:38:00 -0600

    New type of cell for collection view

---
+++ HotFlix/Views/MovieCollectionCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.m
+//  HotFlix
+//
+//  Created by Mercy Bickell on 6/25/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import "MovieCollectionCell.h"
+
+@implementation MovieCollectionCell
+
+@end
