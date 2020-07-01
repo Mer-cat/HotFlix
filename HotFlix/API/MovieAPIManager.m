7988fc3db7e88d1cb2d71f2f6c629628b6903beb
Mercy Bickell <mercycat@fb.com>
2020-07-01 11:21:17 -0600

    add method to fetch popular movies

--- HotFlix/API/MovieAPIManager.m
+++ HotFlix/API/MovieAPIManager.m
@@ -34,7 +34,6 @@
             
             // The network request has completed, but failed.
             // Invoke the completion block with an error.
-            // Think of invoking a block like calling a function with parameters
             completion(nil, error);
         }
         else {
@@ -45,8 +44,33 @@
             
             // The network request has completed, and succeeded.
             // Invoke the completion block with the movies array.
-            // Think of invoking a block like calling a function with parameters
             completion(movies, nil);
+        }
+    }];
+    [task resume];
+}
+
+// Fetches the popular movies rather than the now playing movies
+- (void)fetchPopular:(void(^)(NSMutableArray *popularMovies, NSError *error))completion {
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/popular?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSessionDataTask *task = [self.session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+
+            // The network request has completed, but failed.
+            // Invoke the completion block with an error.
+            completion(nil, error);
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+
+            NSArray *dictionaries = dataDictionary[@"results"];
+            NSMutableArray *popularMovies = [Movie moviesWithDictionaries:dictionaries];
+            
+            // The network request has completed, and succeeded.
+            // Invoke the completion block with the popular movies array.
+            completion(popularMovies, nil);
         }
     }];
     [task resume];
